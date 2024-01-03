a562acbd94fb953b62681aec1192611b8e29a9d3
Anwar Fauzan <muhammad.anwar@mhs.itenas.ac.id>
2024-01-03 10:52:39 +0700

    Benerin typo

--- resources/views/masyarakat/home.blade.php
+++ resources/views/masyarakat/home.blade.php
@@ -66,7 +66,7 @@
                 src="https://mpp.cimahikota.go.id/layanan/1666239444_kisspng-directorate-general-of-immigration-logo-vector-gra-international-affairs-office-5b6d3f3f610287.0532550815338862713974.png"
                 alt="Logo IMIGRASI">
         </div>
-        <h3 class="service-title">iMIGRASI</h3>
+        <h3 class="service-title">IMIGRASI</h3>
     </a>
     <a href="https://mpp.cimahikota.go.id/layanan/1" class="box">
         <div class="logo-container">
@@ -101,7 +101,7 @@
             <img class="logo" src="https://mpp.cimahikota.go.id/layanan/1666239844_Lambang_Polda_Jabar.png"
                 alt="Logo POLRES">
         </div>
-        <h3 class="service-title">KPOLRES</h3>
+        <h3 class="service-title">POLRES</h3>
     </a>
     <a href="https://mpp.cimahikota.go.id/layanan/1" class="box">
         <div class="logo-container">
