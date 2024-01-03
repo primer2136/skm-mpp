be287d65b1dd18b8838b8c8fda42713514e58251
Anwar Fauzan <muhammad.anwar@mhs.itenas.ac.id>
2024-01-03 10:09:06 +0700

    Beranda Done2

--- resources/views/masyarakat/layout/master.blade.php
+++ resources/views/masyarakat/layout/master.blade.php
@@ -27,7 +27,7 @@
     <nav class="navbar navbar-expand-lg navbar-light mt-3 fixed-top" id="navbar">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          <img src="{{ asset('assets/img/logopengaduan.png') }}" height="30px">
+          <img src="{{ asset('assets/img/logoMPP.png') }}" height="75px">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
