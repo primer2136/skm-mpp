ae58cd00d35870a9189fd0a6f1f309e45dbbaede
M Ramdhani Dinhar Akhadin <ramdhanidinhar2136@gmail.com>
2024-01-03 10:41:35 +0700

    Resolve merge conflict on survey.blade.php

---
+++ resources/views/survey.blade.php
@@ -0,0 +1,17 @@
+@extends('masyarakat/layout/navbar')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>SURVEY KEPUASAN MASYARAKAT</title>
+</head>
+<body>
+  <div class="container">
+    <h1>SURVEY KEPUASAN MASYARAKAT</h1>
+    <div class="box">
+      <p>pertanyaan.</p>
+    </div>
+  </div>
+</body>
+</html>
