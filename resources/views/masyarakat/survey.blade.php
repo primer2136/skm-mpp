216967ceaae288d0ee33b46ae4bdc3b3a7987edf
M Ramdhani Dinhar Akhadin <ramdhanidinhar2136@gmail.com>
2024-01-11 10:54:07 +0700

    asek

--- resources/views/masyarakat/survey.blade.php
+++ resources/views/masyarakat/survey.blade.php
@@ -59,7 +59,7 @@
                 <option value="wirausaha">Wirausaha</option>
                 <option value="lainnya">Lainnya</option>
             </select><br><br>
-            <button class="btn-back" onclick="history.back()">Kembali</button>
+            <button class="btn-back" type="button" onclick="goback()">Kembali</button>
             <button type="button" onclick="tampilkanPertanyaan()">Lanjutkan</button>
         </form>
 
@@ -77,13 +77,14 @@
                         value=2>
                     <span>Kurang Mudah</span>
                 </label>
-                <label class="radio-label" for="answer_1_3"> <input type="radio" id="answer_1_3" name="answer_1" value=3>
+                <label class="radio-label" for="answer_1_3"> <input type="radio" id="answer_1_3" name="answer_1"
+                        value=3>
                     <span>Mudah</span>
                 </label>
-                <label class="radio-label" for="answer_1_4"> <input type="radio" id="answer_1_4" name="answer_1" value=4>
+                <label class="radio-label" for="answer_1_4"> <input type="radio" id="answer_1_4" name="answer_1"
+                        value=4>
                     <span>Sangat Mudah</span>
                 </label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
                 <button type="button"
                     onclick="tampilkanPertanyaanSelanjutnya('question_1', 'question_2')">Selanjutnya</button>
             </div>
@@ -91,15 +92,20 @@
                 <!-- Pertanyaan 2 -->
                 <p>Bagaimana Kemudahan Prosedur Pelayanan?</p>
 
-                <label for="answer_2_1"> <input type="radio" id="answer_2_1" name="answer_2" value=1> Tidak
-                    Mudah</label>
-                <label for="answer_2_2"> <input type="radio" id="answer_2_2" name="answer_2" value=2> Kurang
-                    Mudah</label>
-                <label for="answer_2_3"> <input type="radio" id="answer_2_3" name="answer_2" value=3>
-                    Mudah</label>
-                <label for="answer_2_4"> <input type="radio" id="answer_2_4" name="answer_2" value=4> Sangat
-                    Mudah</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_2_1"> <input type="radio" id="answer_2_1" name="answer_2"
+                        value=1> <span>Tidak
+                        Mudah</span>
+                </label>
+                <label class="radio-label" for="answer_2_2"> <input type="radio" id="answer_2_2" name="answer_2"
+                        value=2> <span>Kurang
+                        Mudah</span>
+                </label>
+                <label class="radio-label" for="answer_2_3"> <input type="radio" id="answer_2_3" name="answer_2"
+                        value=3>
+                    <span>Mudah</span></label>
+                <label class="radio-label" for="answer_2_4"> <input type="radio" id="answer_2_4" name="answer_2"
+                        value=4> <span>Sangat
+                        Mudah</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_2', 'question_1')">Kembali</button>
                 <button type="button"
@@ -108,15 +114,18 @@
             <div class="question" id="question_3" style="display: none;">
                 <!-- Pertanyaan 3 -->
                 <p>Bagaimana Kecepatan Pelayanan?</p>
-                <label for="answer_3_1"> <input type="radio" id="answer_3_1" name="answer_3" value=1> Tidak
-                    Cepat</label>
-                <label for="answer_3_2"> <input type="radio" id="answer_3_2" name="answer_3" value=2> Kurang
-                    Cepat</label>
-                <label for="answer_3_3"> <input type="radio" id="answer_3_3" name="answer_3" value=3>
-                    Cepat</label>
-                <label for="answer_3_4"> <input type="radio" id="answer_3_4" name="answer_3" value=4> Sangat
-                    Cepat</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_3_1"> <input type="radio" id="answer_3_1" name="answer_3"
+                        value=1> <span>Tidak
+                        Cepat</span></label>
+                <label class="radio-label" for="answer_3_2"> <input type="radio" id="answer_3_2" name="answer_3"
+                        value=2><span>Kurang
+                        Cepat</span></label>
+                <label class="radio-label" for="answer_3_3"> <input type="radio" id="answer_3_3" name="answer_3"
+                        value=3><span>Cepat</span>
+                </label>
+                <label class="radio-label" for="answer_3_4"> <input type="radio" id="answer_3_4" name="answer_3"
+                        value=4><span>Sangat
+                        Cepat</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_3', 'question_2')">Kembali</button>
                 <button type="button"
@@ -125,15 +134,18 @@
             <div class="question" id="question_4" style="display: none;">
                 <!-- Pertanyaan 4 -->
                 <p>Apakah Pelayanan dipungut Biaya?</p>
-                <label for="answer_4_1"> <input type="radio" id="answer_4_1" name="answer_4" value=1> Sangat
-                    Sering</label>
-                <label for="answer_4_2"> <input type="radio" id="answer_4_2" name="answer_4" value=2>
-                    Sering</label>
-                <label for="answer_4_3"> <input type="radio" id="answer_4_3" name="answer_4" value=3>
-                    Sesekali</label>
-                <label for="answer_4_4"> <input type="radio" id="answer_4_4" name="answer_4" value=4> Tidak
-                    Pernah</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_4_1"> <input type="radio" id="answer_4_1" name="answer_4"
+                        value=1><span>Sangat
+                        Sering</span></label>
+                <label class="radio-label" for="answer_4_2"> <input type="radio" id="answer_4_2" name="answer_4"
+                        value=2><span>Sering</span>
+                </label>
+                <label class="radio-label" for="answer_4_3"> <input type="radio" id="answer_4_3" name="answer_4"
+                        value=3>
+                    <span>Sesekali</span></label>
+                <label class="radio-label" for="answer_4_4"> <input type="radio" id="answer_4_4" name="answer_4"
+                        value=4><span>Tidak
+                        Pernah</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_4', 'question_3')">Kembali</button>
                 <button type="button"
@@ -143,15 +155,18 @@
             <div class="question" id="question_5" style="display: none;">
                 <!-- Pertanyaan 5 -->
                 <p>Bagaimana Kualitas Administratif Pelayanan?</p>
-                <label for="answer_5_1"> <input type="radio" id="answer_5_1" name="answer_5" value=1> Tidak
-                    Berkualitas</label>
-                <label for="answer_5_2"> <input type="radio" id="answer_5_2" name="answer_5" value=2> Kurang
-                    Berkualitas</label>
-                <label for="answer_5_3"> <input type="radio" id="answer_5_3" name="answer_5" value=3>
-                    Berkualitas</label>
-                <label for="answer_5_4"> <input type="radio" id="answer_5_4" name="answer_5" value=4> Sangat
-                    Berkualitas</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_5_1"> <input type="radio" id="answer_5_1" name="answer_5"
+                        value=1><span>Tidak
+                        Berkualitas</span></label>
+                <label class="radio-label" for="answer_5_2"> <input type="radio" id="answer_5_2" name="answer_5"
+                        value=2>
+                    <span>Kurang Berkualitas</span></label>
+                <label class="radio-label" for="answer_5_3"> <input type="radio" id="answer_5_3" name="answer_5"
+                        value=3>
+                    <span>Berkualitas</span></label>
+                <label class="radio-label" for="answer_5_4"> <input type="radio" id="answer_5_4" name="answer_5"
+                        value=4><span>Sangat
+                        Berkualitas</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_5', 'question_4')">Kembali</button>
                 <button type="button"
@@ -161,15 +176,18 @@
             <div class="question" id="question_6" style="display: none;">
                 <!-- Pertanyaan 6 -->
                 <p>Bagaimana Kemampuan Petugas Memberikan Pelayanan?</p>
-                <label for="answer_6_1"> <input type="radio" id="answer_6_1" name="answer_6" value=1> Tidak
-                    Kompeten</label>
-                <label for="answer_6_2"> <input type="radio" id="answer_6_2" name="answer_6" value=2> Kurang
-                    Kompeten</label>
-                <label for="answer_6_3"> <input type="radio" id="answer_6_3" name="answer_6" value=3>
-                    Kompeten</label>
-                <label for="answer_6_4"> <input type="radio" id="answer_6_4" name="answer_6" value=4> Sangat
-                    Kompeten</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_6_1"> <input type="radio" id="answer_6_1" name="answer_6"
+                        value=1> <span>Tidak
+                        Kompeten</span></label>
+                <label class="radio-label" for="answer_6_2"> <input type="radio" id="answer_6_2" name="answer_6"
+                        value=2> <span>Kurang
+                        Kompeten</span></label>
+                <label class="radio-label" for="answer_6_3"> <input type="radio" id="answer_6_3" name="answer_6"
+                        value=3>
+                    <span>Kompeten</span></label>
+                <label class="radio-label" for="answer_6_4"> <input type="radio" id="answer_6_4" name="answer_6"
+                        value=4> <span>Sangat
+                        Kompeten</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_6', 'question_5')">Kembali</button>
                 <button type="button"
@@ -179,15 +197,18 @@
             <div class="question" id="question_7" style="display: none;">
                 <!-- Pertanyaan 7 -->
                 <p>Bagaimana Perilaku Petugas Pemberi Layanan?</p>
-                <label for="answer_7_1"> <input type="radio" id="answer_7_1" name="answer_7" value=1> Tidak Sopan
-                    dan Ramah</label>
-                <label for="answer_7_2"> <input type="radio" id="answer_7_2" name="answer_7" value=2> Kurang Sopan
-                    dan Ramah</label>
-                <label for="answer_7_3"> <input type="radio" id="answer_7_3" name="answer_7" value=3> Sopan dan
-                    Ramah</label>
-                <label for="answer_7_4"> <input type="radio" id="answer_7_4" name="answer_7" value=4> Sangat Sopan
-                    dan Ramah</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_7_1"> <input type="radio" id="answer_7_1" name="answer_7"
+                        value=1> <span>Tidak Sopan
+                        dan Ramah</span></label>
+                <label class="radio-label" for="answer_7_2"> <input type="radio" id="answer_7_2" name="answer_7"
+                        value=2> <span>Kurang Sopan
+                        dan Ramah</span></label>
+                <label class="radio-label" for="answer_7_3"> <input type="radio" id="answer_7_3" name="answer_7"
+                        value=3> <span>Sopan dan
+                        Ramah</span></label>
+                <label class="radio-label" for="answer_7_4"> <input type="radio" id="answer_7_4" name="answer_7"
+                        value=4> <span>Sangat Sopan dan Ramah</span>
+                </label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_7', 'question_6')">Kembali</button>
                 <button type="button"
@@ -197,15 +218,18 @@
             <div class="question" id="question_8" style="display: none;">
                 <!-- Pertanyaan 8 -->
                 <p>Bagaimana Kualitas Sarana dan Prasarana Layanan?</p>
-                <label for="answer_8_1"> <input type="radio" id="answer_8_1" name="answer_8" value=1> Tidak
-                    Nyaman</label>
-                <label for="answer_8_2"> <input type="radio" id="answer_8_2" name="answer_8" value=2> Cukup
-                    Nyaman</label>
-                <label for="answer_8_3"> <input type="radio" id="answer_8_3" name="answer_8" value=3>
-                    Nyaman</label>
-                <label for="answer_8_4"> <input type="radio" id="answer_8_4" name="answer_8" value=4> Sangat
-                    Nyaman</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_8_1"> <input type="radio" id="answer_8_1" name="answer_8"
+                        value=1> <span>Tidak
+                        Nyaman</span></label>
+                <label class="radio-label" for="answer_8_2"> <input type="radio" id="answer_8_2" name="answer_8"
+                        value=2> <span>Cukup
+                        Nyaman</span></label>
+                <label class="radio-label" for="answer_8_3"> <input type="radio" id="answer_8_3" name="answer_8"
+                        value=3>
+                    <span>Nyaman</span></label>
+                <label class="radio-label" for="answer_8_4"> <input type="radio" id="answer_8_4" name="answer_8"
+                        value=4> <span>Sangat
+                        Nyaman</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_8', 'question_7')">Kembali</button>
                 <button type="button"
@@ -216,15 +240,18 @@
                 <!-- Pertanyaan 9 -->
                 <p>Bagaimana Kelengkapan Media Penanganan dalam Layanan? (Informasi Website, Informasi Persyaratan,
                     Instagram, Banner/Pamflet, dan Media Sosial Lainnya)</p>
-                <label for="answer_9_1"> <input type="radio" id="answer_9_1" name="answer_9" value=1> Sangat Tidak
-                    Lengkap</label>
-                <label for="answer_9_2"> <input type="radio" id="answer_9_2" name="answer_9" value=2> Kurang
-                    Lengkap</label>
-                <label for="answer_9_3"> <input type="radio" id="answer_9_3" name="answer_9" value=3>
-                    Lengkap</label>
-                <label for="answer_9_4"> <input type="radio" id="answer_9_4" name="answer_9" value=4> Sangat
-                    Lengkap</label><br><br><br>
-                <button class="btn-reset" type="button" onclick="resetPilihan()">Reset Pilihan</button>
+                <label class="radio-label" for="answer_9_1"> <input type="radio" id="answer_9_1" name="answer_9"
+                        value=1> <span>Sangat Tidak
+                        Lengkap</span></label>
+                <label class="radio-label" for="answer_9_2"> <input type="radio" id="answer_9_2" name="answer_9"
+                        value=2> <span>Kurang
+                        Lengkap</span></label>
+                <label class="radio-label" for="answer_9_3"> <input type="radio" id="answer_9_3" name="answer_9"
+                        value=3>
+                    <span>Lengkap</span></label>
+                <label class="radio-label" for="answer_9_4"> <input type="radio" id="answer_9_4" name="answer_9"
+                        value=4> <span>Sangat
+                        Lengkap</span></label><br><br><br>
                 <button class="btn-back" type="button"
                     onclick="kembaliKePertanyaanSebelumnya('question_9', 'question_8')">Kembali</button>
                 <button type="button"
@@ -365,18 +392,6 @@
             }
         });
 
-        function resetPilihan() {
-            var questions = document.querySelectorAll('.question');
-            questions.forEach(function(question) {
-                var radioButtons = question.querySelectorAll('input[type="radio"]');
-                radioButtons.forEach(function(radioButton) {
-                    radioButton.checked = false;
-                    radioButton.classList.remove('selected');
-                });
-            });
-        }
-
-
         function kembaliKePertanyaanSebelumnya(currentQuestionId, previousQuestionId) {
             var currentQuestion = document.getElementById(currentQuestionId);
             var previousQuestion = document.getElementById(previousQuestionId);
@@ -415,6 +430,20 @@
                 }
             }, 1000); // Waktu pengurangan 1 detik
 
+        }
+
+        function goback() {
+            var formSurvey = document.getElementById('formSurvey');
+            var formPertanyaan = document.getElementById('formPertanyaan');
+
+            if (formSurvey.style.display === 'none') {
+                // Jika sedang menampilkan pertanyaan, kembali ke formulir survei
+                formSurvey.style.display = 'block';
+                formPertanyaan.style.display = 'none';
+            } else {
+                // Jika sedang menampilkan formulir survei, kembali ke halaman sebelumnya
+                window.history.back();
+            }
         }
     </script>
 
