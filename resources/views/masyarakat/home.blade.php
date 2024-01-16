2aa2007044344188d59fd0e3ac2e0cfdab8785bf
Anwar Fauzan <muhammad.anwar@mhs.itenas.ac.id>
2024-01-16 08:02:14 +0700

    salah

--- resources/views/masyarakat/home.blade.php
+++ resources/views/masyarakat/home.blade.php
@@ -153,7 +153,7 @@
         <br><br><br id="tenant"><br><br><br><br><br>
 
         <div class="mb-8">
-            <form action="/">
+            <form action="/" method="GET">
                 <div class="w-full md:w-1/2 m-auto relative">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
