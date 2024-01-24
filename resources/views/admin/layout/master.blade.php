a117c0ac2e6ac96580858e0f3a6d783b41e8f0e8
Haidar Rahman Kusuma <haidar.rahman.k@gmail.com>
2024-01-24 11:17:46 +0700

    gagalin2

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11">
 
     <!-- CSS Libraries -->
     <link rel="stylesheet" href="../node_modules/ionicons201/css/ionicons.min.css">
