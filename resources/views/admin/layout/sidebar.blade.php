e6f5281fd13055bbc5c641a7f3bf1eff56a98d2c
Haidar Rahman Kusuma <haidar.rahman.k@gmail.com>
2024-01-26 10:13:52 +0700

    gagalin ges

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -14,16 +14,13 @@
 
             {{-- Entry data --}}
             <li class="menu-header">Entri data</li>
-data
             <li class="@yield('admin')"><a class="nav-link" href="/ds-admin"><i class="fas fa-user"></i> <span>Admin</span></a></li>
             <li class="@yield('responden')"><a class="nav-link" href="/responden"><i class="fas fa-user"></i> <span>Responden</span></a></li>
             <li class="@yield('tenant')"><a class="nav-link" href="/tenant"><i class="fas fa-user"></i> <span>Tenant</span></a></li>
             <li class="@yield('pertanyaan')"><a class="nav-link" href="/pertanyaan"><i class="fas fa-user"></i> <span>Pertanyaan</span></a></li>
-            <li class="@yield('publish')"><a class="nav-link" href="/publish"><i class="fas fa-user"></i> <span>Publish</span></a></li>
+            {{-- <li class="@yield('publish')"><a class="nav-link" href="/publish"><i class="fas fa-user"></i> <span>Publish</span></a></li> --}}
 
             {{-- @endif
-=======
-main
             @if (Auth::guard('admin')->user()->role == 'super admin')
                 <li class="@yield('admin')"><a class="nav-link" href="/ds-admin"><i class="fas fa-user"></i> <span>Admin</span></a></li>
                 <li class="@yield('responden')"><a class="nav-link" href="/responden"><i class="fas fa-user"></i> <span>Responden</span></a></li>
@@ -33,15 +30,11 @@ main
                 <li class="@yield('admin')"><a class="nav-link" href="/ds-admin"><i class="fas fa-user"></i> <span>Admin</span></a></li>
             @elseif (Auth::guard('admin')->user()->role == 'admin tenant 2')
                 <li class="@yield('responden')"><a class="nav-link" href="/responden"><i class="fas fa-user"></i> <span>Responden</span></a></li>
-data
             @endif --}}
-=======
-            @endif
-main
 
             {{-- Penilaian --}}
             <li class="menu-header">Penilaian</li>
-            <li class="@yield('penilaian')"><a class="nav-link" href="/penilaian"><i class="fas fa-database"></i> <span>Entri Penilaian</span></a></li>
+            <li class="@yield('publish')"><a class="nav-link" href="/publish"><i class="fas fa-database"></i> <span>Publish</span></a></li>
 
             {{-- Laporan (komentar ini dihilangkan sesuai permintaan) --}}
             {{-- @if (Auth::guard('admin')->user()->status == 'admin') --}}
