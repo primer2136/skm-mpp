806202a90eb13764bbc8be6c696dfa3f19001eb6
Anwar Fauzan <muhammad.anwar@mhs.itenas.ac.id>
2024-01-26 15:15:05 +0700

    pusing

--- app/Http/Controllers/PublishController.php
+++ app/Http/Controllers/PublishController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Survei;
 use App\Models\Responden;
+use App\Models\Hasil;
 
 class PublishController extends Controller
 {
@@ -13,5 +14,12 @@ class PublishController extends Controller
         $surveis = Survei::orderBy('id_survei')->get();
 
         return view('admin.publish.index', compact('surveis'));
+    }
+
+    public function show(string $id_jawaban)
+    {
+        $hasils = Hasil::findOrFail($id_jawaban);
+
+        return view('admin.publish.view', compact('hasils'));
     }
 }
